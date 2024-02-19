30167bf4952002880b77f4afef97692b02482cf4
= <=>
2024-02-19 13:20:33 +0300

    ++Registration.css added

--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -1,12 +1,18 @@
 // RegistrationPage.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import './RegistrationPage.css'; // Stil dosyasını import etmeyi unutmayın
 
 const apiUrl = 'http://localhost:8082';
 
 const RegistrationPage = () => {
-  const [registerData, setRegisterData] = useState({ firstName: '', lastName: '', username: '', email: '', password: '' });
+  const [registerData, setRegisterData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: ''
+  });
 
   const handleRegisterChange = (e) => {
     setRegisterData({ ...registerData, [e.target.name]: e.target.value });
@@ -16,41 +22,31 @@ const RegistrationPage = () => {
     e.preventDefault();
     try {
       const response = await axios.post(`${apiUrl}/register`, registerData);
-      console.log('Kayıt Başarılı', response.data);
-      // Kayıt başarılı olduktan sonra isterseniz başka bir sayfaya yönlendirebilirsiniz.
+      console.log('Registration Successful', response.data);
     } catch (error) {
-      console.error('Kayıt sırasında hata oluştu', error);
+      console.error('Registration Error', error);
     }
   };
 
   return (
-    <Container fluid className="py-5">
-      <Row className="justify-content-center">
-        <Col md={6}>
-          <div className="text-center mb-4">
-            <h2>Kayıt Ol</h2>
+    <div className="registration-container">
+      <div className="registration-form">
+        <h2>Hesabını Oluştur</h2>
+        <p>League of Legends evrenine katıl</p>
+        <form onSubmit={handleRegisterSubmit}>
+          <input type="text" name="firstName" value={registerData.firstName} onChange={handleRegisterChange} placeholder="Ad" required />
+          <input type="text" name="lastName" value={registerData.lastName} onChange={handleRegisterChange} placeholder="Soyad" required />
+          <input type="email" name="email" value={registerData.email} onChange={handleRegisterChange} placeholder="e-mail" required />
+          <input type="password" name="password" value={registerData.password} onChange={handleRegisterChange} placeholder="Şifre" required />
+          <div className="terms-container">
+            <input type="checkbox" id="terms" />
+            <label htmlFor="terms">Kullanım koşullarını, müşteri sözleşmesini, gizlilik politikasını okudum ve kabul ediyorum.</label>
           </div>
-          <Form onSubmit={handleRegisterSubmit}>
-            <Form.Group>
-              <Form.Control type="text" name="firstName" value={registerData.firstName} onChange={handleRegisterChange} placeholder="Ad" required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control type="text" name="lastName" value={registerData.lastName} onChange={handleRegisterChange} placeholder="Soyad" required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control type="text" name="username" value={registerData.username} onChange={handleRegisterChange} placeholder="Kullanıcı Adı" required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control type="text" name="email" value={registerData.email} onChange={handleRegisterChange} placeholder="e-mail" required />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control type="password" name="password" value={registerData.password} onChange={handleRegisterChange} placeholder="Şifre" required />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="w-100">Kayıt Ol</Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+          <button type="submit" className="register-btn">Create account</button>
+          <p className="login-link">Zaten bir hesabın var mı? <a href="/login">Giriş yap</a></p>
+        </form>
+      </div>
+    </div>
   );
 }
 
